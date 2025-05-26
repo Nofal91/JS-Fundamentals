25ddf25f59bfe3ff47be413b720fd13994c6da35
Walid Nofal <walidnofal.dev@gmail.com>
2025-05-27 00:28:10 +0300

    j

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,9 @@
+#!/usr/bin/node
 
-
-const n = 0;
-
-if (n === 0) {
+const args = process.argv.slice(2); 
+if (args.length === 0) {
   console.log("No argument");
-} else if (n === 1) {
+} else if (args.length === 1) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
